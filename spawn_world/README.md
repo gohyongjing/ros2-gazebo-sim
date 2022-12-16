4408daba5f68934fd52a38f74c3543c9771e1561
Yong Jing <gohyongjing@gmail.com>
2022-12-16 19:50:29 +0800

    [spawn_world] Add spawn_world package

--- spawn_world/README.md
+++ spawn_world/README.md
@@ -4,9 +4,16 @@ This package launches gazebo and a world based on a world file.
 
 Contains the launch/gazebo.launch.py launch file
 
-Contains the worlds/ world file
+Contains the worlds/world file
 
 ## How to recreate
+- Follow the instructions in the minimal_gazebo package, but naming the package as spawn_world instead
+- Update gazebo.launch.py to spawn a world based on worlds/empty.world
+- Add the URDF for the robot (Edit with your text editor or copy and paste the file
+```
+mkdir worlds
+touch worlds/empty.world
+```
 - Update CMakeLists.txt to install the launch file by adding the following lines
 
 ```
--- spawn_world/launch/gazebo.launch.py
+++ spawn_world/launch/gazebo.launch.py
@@ -1,17 +1,27 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-  gazebo = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([os.path.join(
-          get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-      )
+  pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
+  pkg_share = FindPackageShare(package='spawn_world').find('spawn_world')
+  world_path = os.path.join(pkg_share, 'worlds', 'empty.world')
+
+  # Start Gazebo server
+  gazebo_server = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+    launch_arguments={'world': world_path}.items())
+ 
+  # Start Gazebo client    
+  gazebo_client = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
+  )
 
   return LaunchDescription([
-      gazebo,
+      gazebo_server,
+      gazebo_client,
   ])
 
