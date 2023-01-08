45beb19d9fe8e926d1d6fb9d009213e6cb922d3a
Yong Jing <gohyongjing@gmail.com>
2023-01-08 16:32:06 +0800

    [mobile_robot] Add ros2_control

--- mobile_robot/launch/sim.launch.py
+++ mobile_robot/launch/sim.launch.py
@@ -24,8 +24,22 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
